768d461cf530d8acd2d3b193c9ae2e61b0ccfa7c
LucaElisio <personal.elix@gmail.com>
2024-05-21 19:09:07 +0200

    feat: table

--- index.php
+++ index.php
@@ -48,7 +48,11 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-hotel</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -57,14 +61,45 @@ $hotels = [
 
     <!-- Salvo ogni array dell'array hotels in cur_hotel -->
     <?php foreach ($hotels as $cur_hotel) { ?>
-        <ul>
+        <ul class="d-inline-block">
             <!-- Salvo ogni elemento di cur_hotel in value e lo vado a stampare in un <li> -->
-            <?php foreach($cur_hotel as $value) { ?>
+            <?php foreach ($cur_hotel as $value) { ?>
                 <li><?php echo $value ?></li>
             <?php } ?>
-            
+
         </ul>
     <?php } ?>
+
+
+    <table class="table">
+        <thead>
+            <tr>
+                <?php echo "<th scope='col'>#</th>"?>
+                <?php foreach ($hotels[0] as $key => $cur_hotel) { ?>
+                    <?php echo "<th scope='col'>$key</th>"?>
+                <?php } ?>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $index => $cur_hotel) { ?>
+                <tr>
+                    <?php echo "<td>$index</td>" ?>
+                    <?php foreach ($cur_hotel as $value) { ?>
+                        <?php echo "<td>$value</td>" ?>
+                    <?php } ?>
+                </tr>
+
+
+                <!-- <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> -->
+            <?php } ?>
+        </tbody>
+    </table>
 
 </body>
 
